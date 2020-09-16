a1d4fdee15405ca4165b98f7c85e4ff930b8d571
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2020-09-16 21:22:29 +0900

    Update 행렬의_연산.py

--- 행렬의_연산.py
+++ 행렬의_연산.py
@@ -27,6 +27,10 @@ def det(arr):
     return np.linalg.det(np.array(arr))
 
 
+def cross(arr1, arr2):
+    return np.cross(np.array(arr1), np.array(arr2))
+
+
 print("행렬의 덧셈\n", addition([[1, 2], [2, 3]], [[3, 4], [5, 6]]))
 print("Ax=b에서 x\n", solve([[1, 2, 3], [2, 5, 3], [1, 0, 8]], [5, 3, 17]))
 print("역행렬\n", inverse([[1, 2, 3], [2, 5, 3], [1, 0, 8]]))
@@ -34,4 +38,5 @@ print(
     "행렬의 곱 또는 내적\n",
     dot([[1, 2, 4], [2, 6, 0]], [[4, 1, 4, 3], [0, -1, 3, 1], [2, 7, 5, 2]]),
 )
-print("Determinant\n", det([[0, 1, 5], [3, -6, 9], [2, 6, 1]]))
+print("Determinant\n", round(det([[2, 1, -1], [0, 4, -3], [1, -2, 1]])))
+print("Cross Product\n", cross([0, 2, 3], [-2, 2, -1]))
